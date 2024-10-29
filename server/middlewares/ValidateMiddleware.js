0e842dcc52628e8357c80ace0358bb3da9983a42
Mạnh Đạt <tranhuudats1@gmail.com>
2024-10-29 15:19:50 +0700

    feat[dat-14]: API - login & register

---
+++ server/middlewares/ValidateMiddleware.js
@@ -0,0 +1,20 @@
+const validateRegistration = (req, res, next) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  next();
+};
+
+const validateLogin = (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+  next();
+};
+
+module.exports = {
+  validateRegistration,
+  validateLogin,
+};
