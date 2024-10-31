235b4c5bb28f28b898219db5138d4013cfbb65d9
Mạnh Đạt <tranhuudats1@gmail.com>
2024-10-31 11:01:19 +0700

    fix[dat-14]: API-login and register

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,11 +1,11 @@
-const router = require("express").Router();
-const UserController = require("../controllers/UserController");
+const router = require('express').Router();
+const UserController = require('../controllers/UserController');
 const {
   validateRegistration,
   validateLogin,
-} = require("../middlewares/ValidateMiddleware");
+} = require('../middlewares/ValidateMiddleware');
 
-router.post("/register", validateRegistration, UserController.registerUser);
-router.post("/login", validateLogin, UserController.loginUser);
+router.post('/register', validateRegistration, UserController.registerUser);
+router.post('/login', validateLogin, UserController.loginUser);
 
 module.exports = router;
