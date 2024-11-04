af4fcfe2820173ab522196af990e023359b9a4f6
Doan Quoc Hoa <109152800+quochoa304@users.noreply.github.com>
2024-11-04 11:00:52 +0700

    fix[22-hoa] format all code

--- js/outh2.js
+++ js/outh2.js
@@ -1,29 +1,26 @@
-const CLIENT_ID = '896387674450-36c0ktb6qabai36ol9271dlh2u1at97k.apps.googleusercontent.com';
+const CLIENT_ID =
+  '896387674450-36c0ktb6qabai36ol9271dlh2u1at97k.apps.googleusercontent.com';
 
-// Cấu hình Google Sign-In
 function initializeGoogleSignIn() {
   google.accounts.id.initialize({
     client_id: CLIENT_ID,
-    callback: handleCredentialResponse
+    callback: handleCredentialResponse,
   });
 
-  // Hiển thị nút đăng ký Google
   google.accounts.id.renderButton(
-    document.getElementById('googleRegisterBtn'), // Sử dụng ID đúng
-    { theme: 'outline', size: 'large' } // Tùy chọn cho nút
+    document.getElementById('googleRegisterBtn'),
+    { theme: 'outline', size: 'large' }
   );
 }
 
-// Xử lý phản hồi từ Google
 async function handleCredentialResponse(response) {
   const token = response.credential;
-  console.log("Google ID Token:", token);
+  console.log('Google ID Token:', token);
 
-  // Giải mã token để lấy thông tin email
   const userInfo = jwt_decode(token);
-  const username = userInfo.sub; // Dùng sub làm username
-  const email = userInfo.sub; // Cần lấy email thực từ userInfo nếu có
-  const password = userInfo.sub; // Cần xử lý password theo cách tốt hơn
+  const username = userInfo.sub;
+  const email = userInfo.sub;
+  const password = userInfo.sub;
 
   try {
     // Đăng ký người dùng mới
@@ -63,13 +60,12 @@ async function handleCredentialResponse(response) {
           password,
         }),
       });
-      
+
       data = await response.json();
       console.log('User ID:', data.data.userId);
       sessionStorage.setItem('userID', data.data.userId);
       window.location.href = '/pages/chatPage.html';
     } else {
-      // Nếu đăng ký không thành công, thử đăng nhập
       response = await fetch(`${BASE_URL}/api/users/login`, {
         method: 'POST',
         headers: {
@@ -80,7 +76,7 @@ async function handleCredentialResponse(response) {
           password,
         }),
       });
-      
+
       data = await response.json();
       console.log('User ID:', data.data.userId);
       sessionStorage.setItem('userID', data.data.userId);
@@ -91,7 +87,6 @@ async function handleCredentialResponse(response) {
   }
 }
 
-// Đảm bảo DOM đã tải xong trước khi gán các sự kiện
 document.addEventListener('DOMContentLoaded', function () {
-  initializeGoogleSignIn(); // Gọi hàm để hiển thị nút ngay khi DOM đã tải xong
+  initializeGoogleSignIn();
 });
